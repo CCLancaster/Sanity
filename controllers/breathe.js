e44840189abc394f0b75d91e419356db994fa0e9
CCLancaster <connieclancaster@gmail.com>
2020-03-02 03:26:27 -0800

    added breathe static pages and router, more bootstrap futzing

--- controllers/breathe.js
+++ controllers/breathe.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-// GET /help - shows help static page
+// GET /breathe - shows breathe static page
 router.get('/', (req, res) => {
-    res.render('/breathe');
+    res.render('breathe/breathe');
+})
+
+// GET /breathe/deep - static page with gif
+router.get('/deep', (req, res) => {
+    res.render('breathe/deep');
 })
 
 module.exports = router;
